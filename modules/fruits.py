8d4e1b96f521dac5c6904e5ca5d1cadc16affab7
Muktaka Joshipura <muktaka2000@gmail.com>
2017-01-30 18:19:55 +0530

    Added fruit freezing and unfreezing functionality.

--- modules/fruits.py
+++ modules/fruits.py
@@ -1,17 +1,40 @@
 import boardpiece
 import pygame
 import fixedpath
+import colors
 
 class Fruit(boardpiece.BoardPiece):
     def __init__(self, given_board_location, given_board, surface, given_score, given_image_string):
         boardpiece.BoardPiece.__init__(self, given_board_location, given_board, surface)
         self.score = given_score
 
-        self.image = pygame.image.load(given_image_string).convert()
+        self.original_image = pygame.image.load(given_image_string).convert_alpha()
+        self.image = pygame.Surface([self.board.square_side, self.board.square_side], pygame.SRCALPHA)
         self.rect = self.image.get_rect()
 
         self.rect.topleft = self.board.get_position(self.board_location)
         self.draw()
+
+        self.frozen = False
+    
+    def freeze(self):
+        self.frozen = True
+        self.draw(colors.LIGHT_BLUE)
+    
+    def unfreeze(self):
+        self.frozen = False
+        self.draw(colors.SNOW)
+    
+    def draw(self, background_color=colors.SNOW):
+        update_rect = pygame.Rect((0, 0), (self.board.square_side, self.board.square_side))
+
+        pygame.draw.rect(self.image, background_color, update_rect)
+        self.image.blit(self.original_image, (1, 1))
+        pygame.draw.rect(self.image, colors.BLACK, update_rect, 1)
+
+        target_rect = pygame.Rect(update_rect)
+        self.screen.blit(self.image, self.position)
+        pygame.display.update(pygame.Rect(self.position, (self.board.square_side, self.board.square_side)))
 
 class Strawberry(Fruit, fixedpath.FixedPathFollower):
     def __init__(self, given_board_location, given_board, surface, given_score, given_path):
