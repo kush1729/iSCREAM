a25d6fcf6767065cd2567ad30e70d2fc200f78df
Muktaka Joshipura <muktaka2000@gmail.com>
2017-02-02 08:11:18 +0530

    Changed game.py to match new object patterns.

--- modules/game.py
+++ modules/game.py
@@ -2,6 +2,7 @@ import pygame
 import player
 import locations
 import levelparser
+import itertools
 
 import colors
 
@@ -13,31 +14,25 @@ class Game(object):
 
         def fruit_kill_function():
             self.num_fruits -= 1
-            print self.num_fruits
             if self.num_fruits == 0:
                 self.next_wave()
 
-        self.data = levelparser.get_objects(
-            (0, 0), 35, self.screen, fruit_kill_function)
+        self.dataparser = levelparser.Levelparser('monstertest.level.json', (0, 0), self.screen, fruit_kill_function)
         self.not_suspended = True
         self.num_fruits = 0
 
     def start(self):
-        board = self.data[levelparser.BOARD]
-        user = self.data[levelparser.PLAYER]
-        wall_blocks = pygame.sprite.RenderUpdates(
-            self.data[levelparser.WALL_BLOCKS])
-        ice_blocks = pygame.sprite.RenderUpdates(
-            self.data[levelparser.ICE_BLOCKS])
+        board = self.dataparser.board
+        user = self.dataparser.player
+
+        self.dataparser.initiate_monsters()
+        self.dataparser.initiate_blocks()
         self.next_wave()
-        patrolling_monsters = pygame.sprite.RenderUpdates(
-            self.data[levelparser.PATROLLING_MONSTERS])
-        movables = self.data[levelparser.PATROLLING_MONSTERS]
         board.start(user)
 
         clock = pygame.time.Clock()
 
-        for movable in movables:
+        for movable in itertools.chain(self.dataparser.objects[levelparser.PATROLLING_MONSTERS], self.dataparser.objects[levelparser.CHASING_MONSTERS]):
             movable.activate()
 
         pygame.display.flip()
@@ -55,9 +50,8 @@ class Game(object):
             user.kill()
 
     def next_wave(self):
-        self.current_wave = levelparser.get_fruits()
-        self.num_fruits = len(
-            self.current_wave[levelparser.STATIC_FRUITS]) + len(self.current_wave)
+        self.dataparser.next_fruit_wave()
+        self.num_fruits = self.dataparser.get_current_wave_size()
 
     def suspend(self):
         self.not_suspended = False
