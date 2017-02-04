f4d35074a7340d4661ea0ad8f7929ad4caebdbb9
Muktaka Joshipura <muktaka2000@gmail.com>
2017-02-04 22:53:07 +0530

    Added fonts.

---
+++ modules/fonts.py
@@ -0,0 +1,9 @@
+import pygame
+
+pygame.font.init()
+
+SMALL = pygame.font.SysFont("comicsansms", 20)
+MEDIUM = pygame.font.SysFont("comicsansms", 50)
+MED_LARGE = pygame.font.SysFont("comicsansms", 65)
+LARGE = pygame.font.SysFont("comicsansms", 75)
+X_LARGE = pygame.font.SysFont("comicsansms", 130)
