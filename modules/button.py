ad06dc2dbb12cbf556049ef3ca2b23fbe43f60c6
Muktaka Joshipura <muktaka2000@gmail.com>
2017-02-06 17:57:00 +0530

    Formatted using autopep8

--- modules/button.py
+++ modules/button.py
@@ -1,7 +1,10 @@
 import pygame
 import events
 import fonts
+
+
 class Button(object):
+
     def __init__(self, text, rect, color, text_color, callback, screen):
         self.rect = rect
         self.image = pygame.Surface((rect.width, rect.height))
@@ -10,13 +13,14 @@ class Button(object):
         text = fonts.SMALL.render(text, True, text_color)
         text_rect = text.get_rect()
         text_rect.center = self.image.get_rect().center
-        pygame.draw.rect(self.image, color, pygame.Rect(0, 0, rect.width, rect.height))
+        pygame.draw.rect(self.image, color, pygame.Rect(
+            0, 0, rect.width, rect.height))
         self.image.blit(text, text_rect)
-    
+
     def display(self):
         self.screen.blit(self.image, self.rect)
         pygame.display.update(self.rect)
         events.add_click_listener(self, self.callback)
-    
+
     def clean(self):
         events.remove_click_listener(self)
