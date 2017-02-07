c74c396c7149c02a686bed88ae2307aa6546db23
Muktaka Joshipura <muktaka2000@gmail.com>
2017-02-08 03:00:54 +0530

    Implemented pausing and monster waves
    Large update in fruit_kill_function for thread safety.
    
    Implemented clock ticker
    
    Allowed game to clean up after exit.
    
    Implemented pausing/resuming.

--- modules/game.py
+++ modules/game.py
@@ -5,20 +5,37 @@ import levelparser
 import itertools
 import events
 import time
+import threading
 
 import colors
 
 
 class Game(object):
 
-	def __init__(self, screen, board_position, file_name, score_callback, end_callback):
+	def __init__(self, screen, board_position, file_name, score_callback, tick_callback, end_callback):
 		self.screen = screen
 
-		def fruit_kill_function():
-			self.num_fruits -= 1
-			score_callback(self.user.score)
+		self.mutex = threading.Lock()
+		self.num_failures = 0
+
+		def fruit_kill_function(fruit):
+			decision = False
+			self.mutex.acquire()
 			try:
+				if fruit in self.dataparser.objects[levelparser.FRUIT_WAVES][self.dataparser.wave_number][levelparser.STATIC_FRUITS] \
+					or fruit in self.dataparser.objects[levelparser.FRUIT_WAVES][self.dataparser.wave_number][levelparser.MOVING_FRUITS]:
+					self.num_fruits -= 1
+					score_callback(self.user.score)
+					self.num_failures = 0
+				else:
+					self.num_failures = 1
 				if self.num_fruits == 0:
+					decision = True
+					self.num_fruits = 169
+			finally:
+				self.mutex.release()
+			try:
+				if decision:
 					self.next_wave()
 			except IndexError:
 				end_callback(True, self.user.score, time.time() - self.start_time)
@@ -27,18 +44,29 @@ class Game(object):
 		
 		def game_kill():
 			self.board.game_not_ended = False
+			events.remove_keypress_listener(pygame.K_p, self.pause_toggle)
+			events.remove_exit_listener(game_kill)
 		
 		def player_dead():
 			self.board.game_not_ended = False
 			end_callback(False, self.user.score, time.time() - self.start_time)
 
 		events.add_exit_listener(game_kill)
+		events.add_keypress_listener(pygame.K_p, self.pause_toggle)
+		self.tick_callback = tick_callback
 		self.dataparser = levelparser.Levelparser(file_name, board_position, self.screen, fruit_kill_function, player_dead)
 		self.not_suspended = True
 		self.num_fruits = 0
 
 	def start(self):
 		self.start_time = time.time()
+
+		def ticker():
+			while self.board.game_not_ended:
+				time.sleep(1)
+				self.tick_callback(time.time() - self.start_time)
+		threading.Timer(0, ticker).start()
+
 		self.board = self.dataparser.board
 		self.user = self.dataparser.player
 
@@ -56,7 +84,10 @@ class Game(object):
 				self.dataparser.objects[levelparser.MONSTER_WAVES][self.dataparser.wave_number][levelparser.RANDOM_MONSTERS]):
 				monster.kill()
 		self.dataparser.next_wave()
-		self.num_fruits = self.dataparser.get_current_wave_size()
+		self.num_fruits = self.dataparser.get_current_wave_size() - self.num_failures
+		self.activate_movables()
+	
+	def activate_movables(self):
 		for movable in itertools.chain(
 			self.dataparser.objects[levelparser.FRUIT_WAVES][self.dataparser.wave_number][levelparser.MOVING_FRUITS],
 			self.dataparser.objects[levelparser.MONSTER_WAVES][self.dataparser.wave_number][levelparser.PATROLLING_MONSTERS],
@@ -64,11 +95,15 @@ class Game(object):
 			self.dataparser.objects[levelparser.MONSTER_WAVES][self.dataparser.wave_number][levelparser.RANDOM_MONSTERS]):
 			movable.activate()
 
-	def suspend(self):
-		self.not_suspended = False
+	def pause_toggle(self):
+		if self.board.game_not_paused:
+			self.pause()
+		else:
+			self.resume()
 
-	def restart(self):
-		pass
+	def pause(self):
+		self.board.game_not_paused = False
 
-	def continue_game(self):
-		self.not_suspended = True
+	def resume(self):
+		self.board.game_not_paused = True
+		self.activate_movables()
