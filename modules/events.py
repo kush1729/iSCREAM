b55eac8265f4450c324673791881862ac301319f
Muktaka Joshipura <muktaka2000@gmail.com>
2017-02-08 02:56:36 +0530

    Formatted and changed events.py
    Removed a dangerous try-catch that could allow errors to pass silently.
    
    Formatted using autopep8

--- modules/events.py
+++ modules/events.py
@@ -12,20 +12,26 @@ event_dict = {
 
 loop = True
 
+
 def add_keypress_listener(key, callback):
     event_dict[pygame.KEYDOWN][key].add(callback)
+
 
 def remove_keypress_listener(key, callback):
     event_dict[pygame.KEYDOWN][key].remove(callback)
 
+
 def add_click_listener(clickable, callback):
     event_dict[pygame.MOUSEBUTTONUP][clickable] = callback
+
 
 def remove_click_listener(clickable):
     del event_dict[pygame.MOUSEBUTTONUP][clickable]
 
+
 def add_exit_listener(callback):
     event_dict[pygame.QUIT].add(callback)
+
 
 def remove_exit_listener(callback):
     try:
@@ -33,65 +39,74 @@ def remove_exit_listener(callback):
     except KeyError:
         pass
 
-count_dict = {pygame.K_UP:None, pygame.K_DOWN:None, pygame.K_LEFT:None, pygame.K_RIGHT:None}
+count_dict = {pygame.K_UP: None, pygame.K_DOWN: None,
+              pygame.K_LEFT: None, pygame.K_RIGHT: None}
+
+
 def handle_event(event):
-    global loop, count_dict
-    try:
-        if event.type == pygame.KEYDOWN:
-            for action in event_dict[pygame.KEYDOWN][event.key]:
-                if event.key in count_dict: count_dict[event.key] = 0
-                action()
-        elif event.type == pygame.MOUSEBUTTONUP:
-            for clickable in event_dict[pygame.MOUSEBUTTONUP].copy():
-                if clickable.rect.topleft[0] <= event.pos[0] <= clickable.rect.topright[0] \
+    global loop
+    if event.type == pygame.KEYDOWN:
+        for action in event_dict[pygame.KEYDOWN][event.key]:
+            if event.key in count_dict:
+                count_dict[event.key] = 0
+            action()
+    elif event.type == pygame.MOUSEBUTTONUP:
+        for clickable in event_dict[pygame.MOUSEBUTTONUP].copy():
+            if clickable.rect.topleft[0] <= event.pos[0] <= clickable.rect.topright[0] \
                     and clickable.rect.topleft[1] <= event.pos[1] <= clickable.rect.bottomright[1]:
-                    event_dict[pygame.MOUSEBUTTONUP][clickable]()
-        elif event.type == pygame.QUIT:
-            for action in event_dict[pygame.QUIT]:
-                action()
-            loop = False
-    except:
-        if event[0] == 'longpress':
-            for action in event_dict[pygame.KEYDOWN][event[1]]:
-                action()
-            return
+                event_dict[pygame.MOUSEBUTTONUP][clickable]()
+    elif event.type == pygame.QUIT:
+        for action in event_dict[pygame.QUIT].copy():
+            action()
+        loop = False
+            
+
 
 def start():
-    global count_dict
     c = pygame.time.Clock()
-    while loop:
-        c.tick(30)
-        #to handle long press
-        keystate = pygame.key.get_pressed()
-        for arrow_key in count_dict:
-            pressed = keystate[arrow_key]
-            if pressed and count_dict[arrow_key] != None: count_dict[arrow_key] += 1
-            else: count_dict[arrow_key] = None
-            if count_dict[arrow_key] > 2:
-                handle_event(("longpress", arrow_key))
-                
-        for e in pygame.event.get():
-            handle_event(e)
+    try:
+        while loop:
+            c.tick(30)
+            # to handle long press
+            keystate = pygame.key.get_pressed()
+            for arrow_key in count_dict:
+                pressed = keystate[arrow_key]
+                if pressed and count_dict[arrow_key] is not None:
+                    count_dict[arrow_key] += 1
+                else:
+                    count_dict[arrow_key] = None
+                if count_dict[arrow_key] > 5:
+                    for action in event_dict[pygame.KEYDOWN][arrow_key]:
+                        action()
+
+            for e in pygame.event.get():
+                handle_event(e)
+    except KeyboardInterrupt:
+        for action in event_dict[pygame.QUIT]:
+            action()
+
 
 def stop():
     global loop
     loop = False
-    
+
 
 if __name__ == '__main__':
     pygame.init()
     a = pygame.display.set_mode((100, 100))
+
     class B(object):
+
         def __init__(self):
             self.rect = a.get_rect()
+
             def click():
                 print 'click'
             add_click_listener(self, click)
-    
+
     b = B()
+
     def a_p():
         print 'a clicked'
-    
+
     add_keypress_listener(pygame.K_a, a_p)
-    
-    
