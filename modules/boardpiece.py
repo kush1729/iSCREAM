d3dccaa9bb8d64f3c1ccb5427607baa04dd1db9c
Muktaka Joshipura <muktaka2000@gmail.com>
2017-02-08 02:55:06 +0530

    Improved collision resolution.

--- modules/boardpiece.py
+++ modules/boardpiece.py
@@ -3,13 +3,16 @@ import pygame
 
 class BoardPiece(pygame.sprite.Sprite):
 
-    def __init__(self, given_board_location, given_board, screen):
+    def __init__(self, given_board_location, given_board, screen, collide_resolver):
         pygame.sprite.Sprite.__init__(self)
 
         self.board = given_board
         self.board_location = given_board_location
 
-        self.board.reserve_location(self.board_location, self)
+        if not self.board.is_empty(self.board_location):
+            collide_resolver(self.board_location)
+        else:
+            self.board.reserve_location(self.board_location, self)
 
         self.position = self.board.get_position(self.board_location)
         self.screen = screen
